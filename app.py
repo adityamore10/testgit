4f96fb88b5be849bcdc0b03631ad50bd92313222
Aditya More <adityamore1003@gmail.com>
2022-10-26 12:27:43 +0530

    these are the changes made by developer2

--- app.py
+++ app.py
@@ -1,2 +1,3 @@
 print("python ek bahot badhiya language hai aur woh bhot asan langunage hai bakki ke comnpariosn mei")
 print("dekh bhai aabhi hamlogo ne first commit kardiya aabhi second commit karne ka time aagya hai ")
+print("Abhi maine developer2 branch mei change kar raha hu")
